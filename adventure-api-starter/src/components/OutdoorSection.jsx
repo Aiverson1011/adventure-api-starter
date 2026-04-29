15a982634f790f6dca920d21385011f57d49aef5
noagortler <noagortler@gmail.com>
2026-04-28 19:00:17 -0700

    Complete OutdoorSection. Update ActivityCard and AdventurePlan text colour

--- adventure-api-starter/src/components/OutdoorSection.jsx
+++ adventure-api-starter/src/components/OutdoorSection.jsx
@@ -5,18 +5,21 @@ import ActivityCard from "./ActivityCard";
 function OutdoorSection({ activities, onAddActivity }) {
   const [difficulty, setDifficulty] = useState("all");
 
-  // TODO: Filter activities so only outdoor activities are shown.
-  // Hint: activity.category === "outdoors"
-  const outdoorActivities = activities;
+  // STEP 1: Filter activities so only outdoor activities are shown.
+  const outdoorActivities = activities.filter((activity => activity.category === "outdoors"));
 
-  // TODO: If difficulty is "all", show all outdoor activities.
-  // Otherwise, filter by activity.difficulty.
-  const filteredActivities = outdoorActivities;
+  // STEP 2: If difficulty is "all", show all outdoor activities (uses useState to 
+  // track the selected difficulty)
+
+  const filteredActivities = difficulty === "all" ? outdoorActivities : outdoorActivities.filter(
+    (activity) => activity.difficulty === difficulty
+  );
 
   return (
     <section className="adventure-section">
       <h2>Outdoor Adventures</h2>
 
+      {/* Step 3: Filter by: "All", "Easy", "Medium", "Hard" */}
       <label htmlFor="outdoor-difficulty">Filter by difficulty: </label>
       <select
         id="outdoor-difficulty"
@@ -30,7 +33,18 @@ function OutdoorSection({ activities, onAddActivity }) {
       </select>
 
       <div className="activity-grid">
-        {/* TODO: Map over filteredActivities and render ActivityCard components. */}
+        
+        {/* Step 4: Map over filteredActivities and render ActivityCard components. */}
+        {/* Step 5: Pass onAddActivity into ActivityCard so Add to Adventure Plan button works*/}
+
+        {filteredActivities.map((activity) => (
+          <ActivityCard
+            key={activity.id}
+            activity={activity}
+            onAddActivity={onAddActivity}
+          />
+
+        ))}
       </div>
     </section>
   );
